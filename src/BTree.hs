603827dd85965d04d58442aa0b8026e2b85ceda4
niil <niilohlin@gmail.com>
2015-04-15 21:23:42 +0200

    BTree zipper

--- src/BTree.hs
+++ src/BTree.hs
@@ -3,19 +3,16 @@ import Data.List.PointedList
 
 data BTree a = Leaf a | Branch (PointedList (BTree a)) deriving (Show, Eq)
 
-instance Functor BTree where
-    fmap f (Leaf a) = Leaf (f a)
-    fmap f (Branch as) = Branch (fmap fmap fmap f as)
+data Crumb a = Crumb [BTree a ] [BTree a] deriving (Show, Eq)
 
-data Cursor a = Cursor (Maybe (Cursor a)) (BTree a) deriving (Show, Eq)
-
-down :: Cursor a -> Cursor a
-down (c@(Cursor _ (Branch (PointedList _ p _)))) = Cursor (Just c) p
-down a = a
-
-up :: Cursor a -> Cursor a
-up (Cursor (Just m) _) = m
-up c = c
+data FileCursor a = FileCursor [Crumb a] (BTree a) deriving (Show, Eq)
 
 
-main = undefined
+down :: FileCursor a -> Maybe (FileCursor a)
+down (FileCursor cs (Branch (PointedList l p r))) = Just $ FileCursor ((Crumb l r):cs) p
+down _ = Nothing
+
+up :: FileCursor a -> Maybe (FileCursor a)
+up (FileCursor ((Crumb l r):cs) p) = Just $ FileCursor cs (Branch (PointedList l p r))
+up _ = Nothing
+
